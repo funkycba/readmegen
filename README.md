d482ac562b84f490816186ad1b63547852d7ad60
Carter <carter@pangweb.com>
2023-06-05 22:41:02 -0700

    final changes

--- README.md
+++ README.md
@@ -1,31 +1,43 @@
-![](https://img.shields.io/badge/license-Stuffed CrustMIT-red)
-# Dash
+![](https://img.shields.io/badge/license-Mozilla-red)
+# Gallery
 Table of Contents:
-1. [ Description ] (#desc)
-2. [ Usage ] (#usge)
-3. [ Questions ] (#Qs)
-4. [ Tests ] (#tests)
-5. [ Contributing ] (#contrib)
-6. [ License ] (#license)
-7. [ Installation ] (#install)
-
-<a name="desc"></a>
-## 1. Description
+1. [Description](#description)
+2. [Usage](#usage)
+3. [Questions](#questions)
+4. [Tests](#tests)
+5. [Contributing](#contribution)
+6. [License](#license)
+7. [Installation](#installation)
 
 
-<a name="usge"></a>
-## 2. 
+## description
+An app containing data for different css styles. Uses inquiries to execute the choice being made.
 
-<a name="Qs"></a>
-## 3. Questions
-    So your motivation is to Jogging
-    You are doing this becauseget fit.
-    The problem you are trying to solve is lose weight
-    What you have learned is nutrition
-    Username: ![funkycba](https://github.com/funkycba)
 
-    <a name="tests"></a>
-    ## 4. Tests
+## usage 
+Can be used to help modify webpages for those who want to create their own unique looking website.
 
-    <a name="contrib"></a>
-   ## 5. Contributing
+## questions
+    So your motivation is to I wanted to help consumers save time in styling their own webpages so they can focus more on other aspects like making sure the page runs smoothly.
+    You are doing this because I felt like making this after some loose inspiration from making a portfolio. The artistic creativity I had with the portfolio made me think of what other decorations I could have done to it..
+    The problem you are trying to solve is The need to set up your own css file which usually takes time especially if you lack familiarity.
+    What you have learned is the different ways of positioning and coloring texts, bubbles, and columns to name a few.
+    Username:[funkycba](https://github.com/funkycba)
+    Email address:carter@pangweb.com
+    
+    
+## tests
+When the index and css are all set up, open the app by right clicking the index and selecting "Open in default browser." Click the links to browse through the different stlyes that are displayed on the page with provided sample texts with styles of their own. The commands from css should be displayed on the top of the page.
+    
+  
+    
+## contribution
+You will first have to set up styles on css. THEN in the index, you will have to create functions that are linked to each style you have set up for the gallery.
+   
+   
+## license
+Mozilla
+   
+   
+## installation
+Install VSC, create an index file and a css file.
--- index.js
+++ index.js
@@ -6,125 +6,130 @@ const questions = [{
   name: "title",
   type: "input",
   message: "Create a title",
-  
+
 },
 {
   type: 'input',
-  name:'description',
-  message:'Enter your description:',
+  name: 'description',
+  message: 'Enter your description:',
 },
 {
   type: 'input',
-  name:'install',
-  message:'Enter instructions on how to install:',
+  name: 'install',
+  message: 'Enter instructions on how to install:',
 },
 {
   type: 'input',
-  name:'usage',
-  message:'Explain the usage:',
+  name: 'usage',
+  message: 'Explain the usage:',
 },
 {
   type: 'input',
-  name:'contrib',
-  message:'Enter guidelines',
+  name: 'contrib',
+  message: 'Enter guidelines',
 },
 {
   type: 'input',
-  name:'test',
-  message:'Enter instructions:',
+  name: 'test',
+  message: 'Enter instructions:',
 },
-  {
-    type: 'input',
-    name: 'motivation',
-    message: 'What was your motivation?',
-  },
-  {
-    type: 'input',
-    name: 'Why',
-    message: 'What made you want to build this project?',
-  },
-  {
-    type: 'input',
-    name: 'solution',
-    message: 'What problem does this solve?',
-  },
-  {
-    type: 'input',
-    name: 'Learning',
-    message: 'What did you learn?',
-  },
-  {
-    type: 'input',
-    name: 'github',
-    message: 'Enter your GitHub Username',
-  },
-  {
-    type: 'input',
-    name: 'linkedin',
-    message: 'Enter your LinkedIn URL.',
-  },
-  {
-    type: 'input',
-    name:'email',
-    message:'What is your email?',
-  },
-  {
+{
+  type: 'input',
+  name: 'motivation',
+  message: 'What was your motivation?',
+},
+{
+  type: 'input',
+  name: 'Why',
+  message: 'What made you want to build this project?',
+},
+{
+  type: 'input',
+  name: 'solution',
+  message: 'What problem does this solve?',
+},
+{
+  type: 'input',
+  name: 'Learning',
+  message: 'What did you learn?',
+},
+{
+  type: 'input',
+  name: 'github',
+  message: 'Enter your GitHub Username',
+},
+{
+  type: 'input',
+  name: 'linkedin',
+  message: 'Enter your LinkedIn URL.',
+},
+{
+  type: 'input',
+  name: 'email',
+  message: 'What is your email?',
+},
+{
   name: "license",
   type: "list",
   message: "Choose your license",
-  choices: ["Thin Crust", "Stuffed Crust", "Pan"],
-  },
+  choices: ["Apache", "Eclipse", "Mozilla", "MIT"],
+},
 ];
 
 // TODO: Create a function to write README file
-
+var contriblink = '<a name="contrib"></a>'
 const generateMD = ({ title, description, install, usage, contrib, test, motivation, Why, solution, Learning, github, linkedin, email, license }) =>
-`![](https://img.shields.io/badge/license-${license}MIT-red)
+  `![](https://img.shields.io/badge/license-${license}-red)
 # ${title}
 Table of Contents:
-1. [ Description ] (#desc)
-2. [ Usage ] (#usge)
-3. [ Questions ] (#Qs)
-4. [ Tests ] (#tests)
-5. [ Contributing ] (#contrib)
-6. [ License ] (#license)
-7. [ Installation ] (#install)
+1. [Description](#description)
+2. [Usage](#usage)
+3. [Questions](#questions)
+4. [Tests](#tests)
+5. [Contributing](#contribution)
+6. [License](#license)
+7. [Installation](#installation)
 
-<a name="desc"></a>
-## 1. Description
+
+## description
 ${description}
 
-<a name="usge"></a>
-## 2. 
+
+## usage 
 ${usage}
-<a name="Qs"></a>
-## 3. Questions
+
+## questions
     So your motivation is to ${motivation}
-    You are doing this because${Why}.
+    You are doing this because ${Why}.
     The problem you are trying to solve is ${solution}
     What you have learned is ${Learning}
-    Username:[ ${github}](https://github.com/${github})
-    Email address: ${email}
-    <a name="tests"></a>
-    ## 4. Tests
-    ${test}
-    <a name="contrib"></a>
-   ## 5. Contributing
-    ${contrib}
-   <a name="license"></a>
-   ## 6. License
-    ${license}
-   <a name="install"></a>
-   ## 7. Installation
-   ${install}
+    Username:[${github}](https://github.com/${github})
+    Email address:${email}
+    
+    
+## tests
+${test}
+    
+  
+    
+## contribution
+${contrib}
+   
+   
+## license
+${license}
+   
+   
+## installation
+${install}
 `;
 
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions) // this should run the inquirer prompt.
+  inquirer.prompt(questions) // this should run the inquirer prompt.
     .then((answers) => writeFile('README.md', generateMD(answers)))
-    .then(()=> console.log('Successfully wrote'))
+    .then(() => console.log('Successfully wrote'))
     .catch((err) => console.error(err));
 }
 
